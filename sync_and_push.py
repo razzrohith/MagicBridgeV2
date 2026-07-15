bae78a1c26ef03580c9e08363b78d0b85dac2f95
razzrohith <razzrohith@gmail.com>
2026-07-14 20:39:46 -0500

    make MagicBridgeV2 the default landing (root redirect), rebrand native index/kvm/vnc pages, add terminal+native-KVM fallback links

--- sync_and_push.py
+++ sync_and_push.py
@@ -21,7 +21,8 @@ EDEV = r"E:\Startup\MagicbridgeV2"
 EXCLUDE_FILES = ["*_log.txt", "_*.html", "inspect_git.py", "inspect_pi.py",
                  "fix_perms.py", "fix_meta.py", "check_kvmd.py", "deploy_nginx.py",
                  "deploy_index.py", "pull_assets.py", "pull_check.py", "get_janus.py",
-                 "validate_backend.py", "rebrand_login.py"]
+                 "validate_backend.py", "rebrand_login.py", "inspect_default.py",
+                 "rebrand_native.py", "find_terminal.py"]
 
 
 def log(m):
